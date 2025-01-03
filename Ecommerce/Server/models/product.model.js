3b276d94bef72819b8b6c2c1b66d70e9371e0275
Ron Manguerra <ronforrestron24@gmail.com>
2025-01-03 20:04:04 +0800

    feat: product schema

---
+++ Ecommerce/Server/models/product.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    trim: trim,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default ProductSchema;
