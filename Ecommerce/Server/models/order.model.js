077eaf9d7bbcfef3c478cbdc99a896e901dfce31
Ron Manguerra <ronforrestron24@gmail.com>
2025-01-05 19:36:25 +0800

    feat: order.model

---
+++ Ecommerce/Server/models/order.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "product",
+        required: true,
+      },
+    },
+  ],
+});
