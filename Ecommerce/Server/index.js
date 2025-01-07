0200b7609da0dbb2a970f7ec3916ba696f9116a1
Ron Manguerra <ronforrestron24@gmail.com>
2025-01-07 20:56:51 +0800

    feat: routes update

--- Ecommerce/Server/index.js
+++ Ecommerce/Server/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 9024;
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api/v1/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
