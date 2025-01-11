73ae39e91d6eeb21208d772357b922027fc3c320
Ron Manguerra <ronforrestron24@gmail.com>
2025-01-11 20:13:30 +0800

    feat: jwt secret key update for registration

--- Ecommerce/Server/routes/user.route.js
+++ Ecommerce/Server/routes/user.route.js
@@ -3,6 +3,9 @@ import userController from "../controllers/user.controller.js";
 
 const router = express.Router();
 
+router.get("/", () => {
+  console.log("user route");
+});
 router.post("/register", userController.register);
 router.post("/login", userController.loginUser);
 
