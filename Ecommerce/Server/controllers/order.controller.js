c4f9f115bc1bc274f80c5c8d405a917cd5c8bb53
Ron Manguerra <ronforrestron24@gmail.com>
2025-01-11 20:37:08 +0800

    feat: order controller and model

---
+++ Ecommerce/Server/controllers/order.controller.js
@@ -0,0 +1,24 @@
+import Order from "../models/order.model.js";
+
+const orderController = {
+  createOrder: async (req, res) => {
+    try {
+      const { userId, items, totalPrice } = req.body;
+
+      const newOrder = new Order({
+        user,
+        items,
+        totalPrice,
+        status: "Pending",
+      });
+
+      await newOrder.save();
+
+      res
+        .status(201)
+        .send({ message: "order placed successfully", order: newOrder });
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  },
+};
