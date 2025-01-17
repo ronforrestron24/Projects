3c11dbd3a01c9227f298d2ea99c9072e4f162481
Ron Manguerra <ronforrestron24@gmail.com>
2025-01-17 18:25:49 +0800

    feat: tsecommerce

--- EcommerceTS/src/index.ts
+++ EcommerceTS/src/index.ts
@@ -1,17 +1,19 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//middlewares
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+connectDB();
+
+app.use("/", (req, res) => {
   console.log("from express and ts");
 });
 
---
+++ EcommerceTS/src/routes/product.route.ts
@@ -0,0 +1,2 @@
+import { Express } from "express";
+import Product from "../models/product.model";
