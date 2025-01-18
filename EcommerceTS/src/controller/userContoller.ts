4712de89e3da6ba80d2b1d4be3366ed7edfdaec7
Ron Manguerra <ronforrestron24@gmail.com>
2025-01-18 10:11:22 +0800

    feat: typescript backend

--- EcommerceTS/src/controller/userContoller.ts
+++ EcommerceTS/src/controller/userContoller.ts
@@ -1,7 +1,5 @@
 import User from "../models/user.models";
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 
 interface IUser {
   name: string;
@@ -12,7 +10,15 @@ interface IUser {
 
 const userController = {
   createUser: async (req: Request, res: Response) => {
-    const { name, email, password, role } = req.body;
+    try {
+      const { name, email, password, role } = req.body;
+
+      const user = await User.create({ name, email, password, role });
+
+      res.status(201).json({ message: "User created successfully", user });
+    } catch (error) {
+      res.status(500).json({ message: "Error creating user", error });
+    }
   },
 };
 
